cfff8efe28790043a1709d8867d8f1e66dd6fd9a
Rosie Faulkner <faulknerproject@gmail.com>
2021-12-22 18:10:51 -0500

    adds swagger ui

--- src/www/index.php
+++ src/www/index.php
@@ -1,60 +1,58 @@
 <?php
 
-use Psr\Http\Message\ServerRequestInterface as Request;
-use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
-use Slim\Factory\AppFactory;
-use Slim\Psr7\Response;
-use Firebase\JWT\JWT;
-use Firebase\JWT\Key;
-
-require('../inc/bootstrap.php');
-
-//Load env
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../');
-$dotenv->load();
-
-$app = AppFactory::create();
-
-/*
- * Add Error Handling Middleware
- *
- * @param bool $displayErrorDetails -> Should be set to false in production
- * @param bool $logErrors -> Parameter is passed to the default ErrorHandler
- * @param bool $logErrorDetails -> Display error details in error log
- * which can be replaced by a callable of your choice.
-
- * Note: This middleware should be added last. It will not handle any exceptions/errors
- * for middleware added after it.
- */
-$errorMiddleware = $app->addErrorMiddleware(true, true, true);
-
 /**
- * Middlewares
+ * @OA\Server(url="http://localhost:8888")
+ * @OA\Info(title="wf-api", version="1.0.0")
  */
-// Parse json, form data and xml
-// Define app routes
-$app->add(CorsMiddleware::class);
-$app->addBodyParsingMiddleware();
-$app->addRoutingMiddleware();
-$container = $app->getContainer();
-$rawPublicKeys = file_get_contents('https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-odgy7%40wf-api-ccdfe.iam.gserviceaccount.com');
-$keys = json_decode($rawPublicKeys, true);
-$app->add(new \Tuupola\Middleware\JwtAuthentication([
-	"algorithm" => ["HS256"],
-	"header" => "X-Authorization",
-	"secret" => $keys,
-	"secure" => false,
-	"error" => function ($response, $arguments) {
-		$data["status"] = "error";
-		$data["message"] = $arguments["message"];
-		return $response
-			->withHeader("Content-Type", "application/json")
-			->getBody()->write(json_encode($data, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
-	},
-    "callback" => function ($request, $response, $arguments) use ($container) {
-        $container["jwt"] = $arguments["decoded"];
-    }
-]));
 
-// Run app
-$app->run();
+use Firebase\JWT\JWT;
+use Slim\Factory\AppFactory;
+
+require $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
+class Api
+{
+	private $key = 'privatekey';
+	public function __construct()
+	{
+		$this->middleware();
+		$this->routes();
+	}
+	//Define middleware
+	public function middleware()
+	{
+		$app = AppFactory::create();
+		return $app->addErrorMiddleware(true, true, true);
+	}
+	/**
+	 * @OA\Get(path="/src/www/index/routes", tags={'Api'},
+	 * @OA\Response (response="200", description="Success"),
+	 * @OA\Response (response="404", description="Not Found"),
+	 * )
+	 */
+	// Define app routes
+	public function routes()
+	{
+		$app = AppFactory::create();
+		$app->get('/', function (Request $request, Response $response) {
+			$response->getBody()->write('Hello world!');
+			return $response;
+		});
+	}
+	public function auth()
+	{
+		$iat = time();
+		$exp = $iat + 60 * 60;
+		$payload = [
+			'iss' => 'http://localhost:8888', //issuer
+			'aud' => 'http://localhost:8888/', //audience
+			'iat' => $iat, //time jwt was issued
+			'exp' => $exp //time jwt expires
+		];
+		$jwt = JWT::encode($payload, $this->key, 'HS512');
+		return [
+			'token' => $jwt,
+			'expires' => $exp
+		];
+	}
+}
+$api = new Api();
